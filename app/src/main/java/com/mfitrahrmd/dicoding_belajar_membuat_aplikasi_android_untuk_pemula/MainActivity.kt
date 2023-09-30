d382786d8a97cc51917e75d708fcf50fae7292eb
mfitrahrmd <tgcfitrah26@gmail.com>
2023-09-30 13:35:32 +0700

    implement login calculate volume

--- app/src/main/java/com/mfitrahrmd/dicoding_belajar_membuat_aplikasi_android_untuk_pemula/MainActivity.kt
+++ app/src/main/java/com/mfitrahrmd/dicoding_belajar_membuat_aplikasi_android_untuk_pemula/MainActivity.kt
@@ -2,10 +2,31 @@ package com.mfitrahrmd.dicoding_belajar_membuat_aplikasi_android_untuk_pemula
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.mfitrahrmd.dicoding_belajar_membuat_aplikasi_android_untuk_pemula.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var _mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_mainBinding.root)
+
+        _mainBinding.btnCalculate.setOnClickListener {
+            val length = _mainBinding.etLength.text.toString().toDouble()
+            val width = _mainBinding.etWidth.text.toString().toDouble()
+            val height = _mainBinding.etHeight.text.toString().toDouble()
+
+            val volume = calculateVolume(length, width, height)
+
+            _mainBinding.tvResult.text = volume.toString()
+        }
+    }
+
+    fun calculateVolume(length: Double, width: Double, height: Double): Double {
+        return length * width * height
     }
 }
