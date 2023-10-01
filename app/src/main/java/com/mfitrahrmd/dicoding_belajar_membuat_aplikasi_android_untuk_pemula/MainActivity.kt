76b945bb350aa9847cb9f90f134fc8bb78591b74
mfitrahrmd <tgcfitrah26@gmail.com>
2023-10-01 12:24:39 +0700

    add viewmodel and live data to observe result data

--- app/src/main/java/com/mfitrahrmd/dicoding_belajar_membuat_aplikasi_android_untuk_pemula/MainActivity.kt
+++ app/src/main/java/com/mfitrahrmd/dicoding_belajar_membuat_aplikasi_android_untuk_pemula/MainActivity.kt
@@ -6,17 +6,26 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.mfitrahrmd.dicoding_belajar_membuat_aplikasi_android_untuk_pemula.databinding.ActivityMainBinding
+
+class MainVM : ViewModel() {
+    val result = MutableLiveData<Int>(0)
+}
 
 class MainActivity : AppCompatActivity() {
     private lateinit var _mainBinding: ActivityMainBinding
+    private lateinit var _mainVM: MainVM
 
     private val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
         if (it.resultCode == MoveForResultActivity.RESULT_CODE) {
             if (it.data != null) {
                 val selectedValue = it.data?.getIntExtra(MoveForResultActivity.EXTRA_SELECTED_VALUE, 0)
 
-                _mainBinding.tvResult.text = "Hasil : $selectedValue"
+                _mainVM.result.postValue(selectedValue)
             }
         }
     }
@@ -26,16 +35,22 @@ class MainActivity : AppCompatActivity() {
         _mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(_mainBinding.root)
 
+        _mainVM = ViewModelProvider(this).get(MainVM::class.java)
+
+        _mainVM.result.observe(this) {
+            _mainBinding.tvResult.text = "Hasil : $it"
+        }
+
         _mainBinding.btnMove.setOnClickListener {
-            move(this@MainActivity)
+            move()
         }
 
         _mainBinding.btnMoveWithData.setOnClickListener {
-            moveWithData(this@MainActivity)
+            moveWithData()
         }
 
         _mainBinding.btnMoveWithObject.setOnClickListener{
-            moveWithObject(this@MainActivity)
+            moveWithObject()
         }
 
         _mainBinding.btnDialNumber.setOnClickListener {
@@ -45,30 +60,30 @@ class MainActivity : AppCompatActivity() {
         }
 
         _mainBinding.btnMoveForResult.setOnClickListener {
-            moveForResult(this@MainActivity)
+            moveForResult()
         }
     }
 
-    private fun moveForResult(ctx: Context) {
-        val moveForResultIntent = Intent(ctx, MoveForResultActivity::class.java)
+    private fun moveForResult() {
+        val moveForResultIntent = Intent(this@MainActivity, MoveForResultActivity::class.java)
         resultLauncher.launch(moveForResultIntent)
     }
 
-    private fun moveWithObject(ctx: Context) {
-        val moveWithObjectIntent = Intent(ctx, MoveWithObjectActivity::class.java)
+    private fun moveWithObject() {
+        val moveWithObjectIntent = Intent(this@MainActivity, MoveWithObjectActivity::class.java)
         moveWithObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, Person("John Doe", 23))
         startActivity(moveWithObjectIntent)
     }
 
-    private fun moveWithData(ctx: Context) {
-        val moveWithDataIntent = Intent(ctx, MoveWithDataActivity::class.java)
+    private fun moveWithData() {
+        val moveWithDataIntent = Intent(this@MainActivity, MoveWithDataActivity::class.java)
         moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "John Doe")
         moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 23)
         startActivity(moveWithDataIntent)
     }
 
-    private fun move(ctx: Context) {
-        val moveIntent = Intent(ctx, MoveActivity::class.java)
+    private fun move() {
+        val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
         startActivity(moveIntent)
     }
 
