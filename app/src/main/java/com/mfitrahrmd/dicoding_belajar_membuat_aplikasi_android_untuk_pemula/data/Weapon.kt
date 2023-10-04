089a8fe42bbfe2c6c17b56729b14428c786724bd
mfitrahrmd <tgcfitrah26@gmail.com>
2023-10-05 00:05:29 +0700

    define weapon class

---
+++ app/src/main/java/com/mfitrahrmd/dicoding_belajar_membuat_aplikasi_android_untuk_pemula/data/Weapon.kt
@@ -0,0 +1,15 @@
+package com.mfitrahrmd.dicoding_belajar_membuat_aplikasi_android_untuk_pemula.data
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Weapon(
+    val name: String,
+    val type: String,
+    val damage: Int,
+    val energyCost: Int,
+    val critChance: Int,
+    val inaccuracy: Int,
+    val imageId: Int
+) : Parcelable
