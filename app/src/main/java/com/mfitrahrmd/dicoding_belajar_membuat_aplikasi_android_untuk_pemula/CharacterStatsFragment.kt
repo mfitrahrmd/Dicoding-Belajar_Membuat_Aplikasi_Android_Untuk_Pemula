063862bdea70f02f5652d99de1efc3b695d81935
mfitrahrmd <tgcfitrah26@gmail.com>
2023-10-05 02:31:52 +0700

    hoist required character data

--- app/src/main/java/com/mfitrahrmd/dicoding_belajar_membuat_aplikasi_android_untuk_pemula/CharacterStatsFragment.kt
+++ app/src/main/java/com/mfitrahrmd/dicoding_belajar_membuat_aplikasi_android_untuk_pemula/CharacterStatsFragment.kt
@@ -6,11 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.mfitrahrmd.dicoding_belajar_membuat_aplikasi_android_untuk_pemula.adapters.WeaponInfoViewPagerAdapter
+import com.mfitrahrmd.dicoding_belajar_membuat_aplikasi_android_untuk_pemula.data.Buff
 import com.mfitrahrmd.dicoding_belajar_membuat_aplikasi_android_untuk_pemula.data.Weapon
 import com.mfitrahrmd.dicoding_belajar_membuat_aplikasi_android_untuk_pemula.databinding.CharacterStatsBinding
 
 
-class CharacterStatsFragment : Fragment() {
+class CharacterStatsFragment(
+    private val _weapons: Array<Weapon>,
+    private val _buff: Buff
+) : Fragment() {
     private lateinit var _binding: CharacterStatsBinding
 
     override fun onCreateView(
@@ -20,12 +24,10 @@ class CharacterStatsFragment : Fragment() {
     ): View? {
         _binding = CharacterStatsBinding.inflate(inflater, container, false)
 
-        _binding.vpWeap.adapter = WeaponInfoViewPagerAdapter(
-            arrayOf(
-                Weapon("Wooden Cross", "Staff", 4, 2, 0, 5, R.drawable.priestess_weapon_0),
-                Weapon("Wooden Cross", "Staff", 4, 2, 0, 5, R.drawable.priestess_weapon_7),
-            )
-        )
+        _binding.tvBuffDesc.text = _buff.description
+        _binding.ivBuff.setImageResource(_buff.imageId)
+
+        _binding.vpWeap.adapter = WeaponInfoViewPagerAdapter(_weapons)
 
         return _binding.root
     }
