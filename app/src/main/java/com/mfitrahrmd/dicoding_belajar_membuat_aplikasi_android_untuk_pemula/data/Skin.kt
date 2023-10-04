c60dea375367a4dae9b6ac0cd40f002d645d747e
mfitrahrmd <tgcfitrah26@gmail.com>
2023-10-05 00:05:43 +0700

    define character skin class

---
+++ app/src/main/java/com/mfitrahrmd/dicoding_belajar_membuat_aplikasi_android_untuk_pemula/data/Skin.kt
@@ -0,0 +1,11 @@
+package com.mfitrahrmd.dicoding_belajar_membuat_aplikasi_android_untuk_pemula.data
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Skin(
+    val name: String,
+    val imageId: Int,
+    val artImageId: Int?
+) : Parcelable
