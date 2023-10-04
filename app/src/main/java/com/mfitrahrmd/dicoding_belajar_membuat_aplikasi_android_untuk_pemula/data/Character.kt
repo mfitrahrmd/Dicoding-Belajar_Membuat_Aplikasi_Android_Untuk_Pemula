ffef2e806e61de7f93dcbe25fcbaf2b2e88890a0
mfitrahrmd <tgcfitrah26@gmail.com>
2023-10-05 02:30:24 +0700

    add character properties

--- app/src/main/java/com/mfitrahrmd/dicoding_belajar_membuat_aplikasi_android_untuk_pemula/data/Character.kt
+++ app/src/main/java/com/mfitrahrmd/dicoding_belajar_membuat_aplikasi_android_untuk_pemula/data/Character.kt
@@ -6,8 +6,30 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class Character(
     val name: String,
-    val specialities: ArrayList<String>,
+    val specialities: Array<String>,
     val info: String,
-    val imageId: Int,
-    val color: Int
-) : Parcelable
+    val color: Int,
+    val buff: Buff,
+    val skins: Array<Skin>,
+    val weapons: Array<Weapon>
+) : Parcelable {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Character
+
+        if (!specialities.contentEquals(other.specialities)) return false
+        if (!skins.contentEquals(other.skins)) return false
+        if (!weapons.contentEquals(other.weapons)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = specialities.contentHashCode()
+        result = 31 * result + skins.contentHashCode()
+        result = 31 * result + weapons.contentHashCode()
+        return result
+    }
+}
