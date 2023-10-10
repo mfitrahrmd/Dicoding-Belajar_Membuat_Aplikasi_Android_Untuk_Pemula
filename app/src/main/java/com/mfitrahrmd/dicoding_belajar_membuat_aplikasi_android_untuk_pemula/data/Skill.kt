14adaf72cf98ec5fe26891e6c6d4707892c03cd4
mfitrahrmd <tgcfitrah26@gmail.com>
2023-10-10 18:35:30 +0700

    define character skill class

---
+++ app/src/main/java/com/mfitrahrmd/dicoding_belajar_membuat_aplikasi_android_untuk_pemula/data/Skill.kt
@@ -0,0 +1,12 @@
+package com.mfitrahrmd.dicoding_belajar_membuat_aplikasi_android_untuk_pemula.data
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Skill(
+    val name: String,
+    val imageId: Int,
+    val basicInfo: String,
+    val detailInfo: String
+) : Parcelable
