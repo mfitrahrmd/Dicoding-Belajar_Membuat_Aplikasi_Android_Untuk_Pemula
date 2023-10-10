4be7c6df17ead1ca8abf41748720041afb74392b
mfitrahrmd <tgcfitrah26@gmail.com>
2023-10-10 18:36:12 +0700

    refactor list

--- app/src/main/java/com/mfitrahrmd/dicoding_belajar_membuat_aplikasi_android_untuk_pemula/data/Character.kt
+++ app/src/main/java/com/mfitrahrmd/dicoding_belajar_membuat_aplikasi_android_untuk_pemula/data/Character.kt
@@ -6,7 +6,7 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class Character(
     val name: String,
-    val specialities: Array<String>,
+    val specialities: List<String>,
     val info: String,
     val health: Int,
     val armor: Int,
@@ -14,26 +14,7 @@ data class Character(
     val damage: Int,
     val color: Int,
     val buff: Buff,
-    val skins: Array<Skin>,
-    val weapons: Array<Weapon>
-) : Parcelable {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Character
-
-        if (!specialities.contentEquals(other.specialities)) return false
-        if (!skins.contentEquals(other.skins)) return false
-        if (!weapons.contentEquals(other.weapons)) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = specialities.contentHashCode()
-        result = 31 * result + skins.contentHashCode()
-        result = 31 * result + weapons.contentHashCode()
-        return result
-    }
-}
+    val skins: List<Skin>,
+    val weapons: List<Weapon>,
+    val skills: List<Skill>
+) : Parcelable
